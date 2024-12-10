317e5d5334599ecb05e76e957de01c69be2a2a30
tackboon <tackboon97@gmail.com>
2024-12-11 02:28:20 +0800

    clean up code

--- web/src/page/auth/forgot.component.tsx
+++ web/src/page/auth/forgot.component.tsx
@@ -3,12 +3,10 @@ import Container from "../../component/container/container.component";
 import ForgotPasswordForm from "../../form/forgot_password/forgot_form.component";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  selectUserError,
   selectUserIsLoading,
 } from "../../store/user/user.selector";
 import { forgotPasswordReset } from "../../store/user/user.action";
 import {
-  USER_ERROR_TYPES,
   USER_LOADING_TYPES,
 } from "../../store/user/user.types";
 import { useEffect } from "react";
